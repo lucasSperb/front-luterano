e3ccc11e3876d113d5089a9595b6b14ec8130689
Lucas Sperb <lucas205.sperb@gmail.com>
2025-11-05 11:26:12 -0300

    Editar Usuario

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Sidebar.css';
 import logo from '../../images/logoPaz.avif';
